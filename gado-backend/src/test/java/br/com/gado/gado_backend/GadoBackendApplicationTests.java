1c364e4ffc7e7ef83feba088353979acd57952d5
Pedro Américo <pedroamericorocha@gmail.com>
2026-03-09 23:09:06 -0300

    Criação projeto backend

---
+++ gado-backend/src/test/java/br/com/gado/gado_backend/GadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gado.gado_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
