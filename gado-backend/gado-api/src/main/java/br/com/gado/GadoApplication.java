c426bf6f8d7fa5cae3f2bbbb8267bc34efa16659
Pedro Américo <pedroamericorocha@gmail.com>
2026-03-10 23:11:07 -0300

    criacao estrutura ddd

---
+++ gado-backend/gado-api/src/main/java/br/com/gado/GadoApplication.java
@@ -0,0 +1,12 @@
+package br.com.gado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadoApplication.class, args);
+    }
+}
