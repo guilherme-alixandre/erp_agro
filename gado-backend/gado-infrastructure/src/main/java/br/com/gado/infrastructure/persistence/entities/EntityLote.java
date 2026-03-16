7931c1a59f8f35751644eecb4f2a2010e2619ff2
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:54:46 -0300

    criada a entidade de Lotes

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityLote.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityLote.java
@@ -1,4 +1,26 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
 public class EntityLote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private String racaPredominante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private EntityUsuario usuario;
+
+    @OneToMany(mappedBy = "lote")
+    private List<EntityTransacao> transacoes = new ArrayList<>();
+
 }
