6e57fd4830b165ed9236c7bfc67737ae80f9d72c
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:55:53 -0300

    criada a entidade para Transações

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityTransacao.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityTransacao.java
@@ -1,4 +1,27 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class EntityTransacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime data;
+    private Double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "parceiro_id")
+    private EntityParceiro parceiro;
+
+    // necessário, depois explico, ligado com EntityLote
+    @ManyToOne
+    @JoinColumn(name = "lote_id")
+    private EntityLote lote;
+
 }
