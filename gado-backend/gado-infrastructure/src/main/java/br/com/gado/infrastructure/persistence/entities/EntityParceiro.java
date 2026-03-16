fe9622d635944ad26c1c721ca5bc36f3a89e8b58
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:55:08 -0300

    criada a entidade para Parceiros

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityParceiro.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityParceiro.java
@@ -1,4 +1,25 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import br.com.gado.domain.enums.ETipoParceiro;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class EntityParceiro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String CPF_CNPJ;
+    private String endereco;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+
+    @Enumerated(EnumType.STRING)
+    private ETipoParceiro tipo;
+
 }
