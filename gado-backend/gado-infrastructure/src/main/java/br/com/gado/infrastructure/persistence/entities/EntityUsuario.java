35abdbfd478a0d7e48a136e2b43ce7e13220670d
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:56:15 -0300

    criada a entidade para Usuários

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityUsuario.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityUsuario.java
@@ -1,4 +1,26 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import br.com.gado.domain.enums.EPerfilUsuario;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class EntityUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    // criarei classe email ainda
+    private String email;
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private EPerfilUsuario perfil;
+
+    private LocalDateTime dataCadastro;
+
 }
