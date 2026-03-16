319738ce560757e73fd76e02382eb5114c9e2498
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:54:21 -0300

    criada a entidade insumo

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityInsumo.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntityInsumo.java
@@ -1,4 +1,27 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import br.com.gado.domain.enums.ETipoInsumo;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class EntityInsumo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Double estoqueMinimo;
+    private Double saldoAtual;
+
+    @ManyToOne
+    @JoinColumn(name = "parceiro_id")
+    private EntityParceiro parceiro;
+
+    @Enumerated(EnumType.STRING)
+    private ETipoInsumo tipo;
+
+    // fazer depois o unidade_medida pq não lembro pra que isso
+
 }
