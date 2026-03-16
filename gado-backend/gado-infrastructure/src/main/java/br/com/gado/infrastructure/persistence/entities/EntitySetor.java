d298a5c7442e6d6e363d3bc2ebc47565c85cd1f3
MrCode-Man <minimina722@gmail.com>
2026-03-16 12:55:29 -0300

    criada a entidade para Setor

--- gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntitySetor.java
+++ gado-backend/gado-infrastructure/src/main/java/br/com/gado/infrastructure/persistence/entities/EntitySetor.java
@@ -1,4 +1,23 @@
 package br.com.gado.infrastructure.persistence.entities;
 
+import br.com.gado.domain.enums.ETipoSetor;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class EntitySetor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int capacidadeMaxima;
+    private String metaTexto; // não lembro o que isso faz, só copiei mesmo
+    private Double metaProducaoLeite;
+    private Double metaArrobaAbate;
+
+    @Enumerated(EnumType.STRING)
+    private ETipoSetor setor;
+
 }
