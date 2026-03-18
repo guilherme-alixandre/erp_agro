22d9d2e4cd0e303ed5a9bb30ee3a707fd32189e4
MrCode-Man <minimina722@gmail.com>
2026-03-18 12:09:24 -0300

    criado o CRUD para o controller animal

--- gado-backend/gado-api/src/main/java/br/com/gado/controllers/CAnimal.java
+++ gado-backend/gado-api/src/main/java/br/com/gado/controllers/CAnimal.java
@@ -1,4 +1,42 @@
 package br.com.gado.controllers;
 
+import br.com.gado.application.services.SAnimal;
+import br.com.gado.dto.AnimalDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class CAnimal {
+
+    private final SAnimal animalService;
+
+    public CAnimal(SAnimal animalService){
+        this.animalService = animalService;
+    }
+
+
+    @GetMapping("/animais/{brinco}")
+    public Map<String, Object> getAnimal(@PathVariable String brinco){
+        return animalService.buscarPorBrinco(brinco);
+    }
+
+    @PostMapping("/animais")
+    public String postAnimal(@RequestBody AnimalDto animal){
+        return animalService.cadastraAnimal(animal);
+    }
+
+    @DeleteMapping("/animais/{brinco}")
+    public String deleteAnimal(@PathVariable String brinco){
+        return animalService.deletaAnimal(brinco);
+    }
+
+    @PutMapping("/animais/{brinco}")
+    public String putAnimal(@PathVariable String brinco,
+                            @RequestBody AnimalDto animal)
+    {
+        return animalService.alteraAnimal(brinco, animal);
+    }
+
 }
