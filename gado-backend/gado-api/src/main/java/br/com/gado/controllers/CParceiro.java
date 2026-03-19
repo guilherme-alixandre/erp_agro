67bdf66c75eed73c0b29afa3f90a5ed5f1ff40fd
MrCode-Man <minimina722@gmail.com>
2026-03-19 12:56:06 -0300

    controller para parceiro criado

--- gado-backend/gado-api/src/main/java/br/com/gado/controllers/CParceiro.java
+++ gado-backend/gado-api/src/main/java/br/com/gado/controllers/CParceiro.java
@@ -1,4 +1,42 @@
 package br.com.gado.controllers;
 
+import br.com.gado.application.services.SParceiro;
+import br.com.gado.dto.parcerioDto.ParceiroCadastroDto;
+import br.com.gado.dto.parcerioDto.ParceiroPutDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/parceiros")
 public class CParceiro {
+
+    private final SParceiro parceiroService;
+
+    public CParceiro(SParceiro parceiroService){
+        this.parceiroService = parceiroService;
+    }
+
+
+    @GetMapping("/{cpf_cnpj}")
+    public Map<String, Object> getParceiro(@PathVariable String cpf_cnpj){
+        return parceiroService.buscaPorCPF_CNPJ(cpf_cnpj);
+    }
+
+    @PostMapping("/")
+    public String postParceiro(@RequestBody ParceiroCadastroDto dto){
+        return parceiroService.cadastra(dto);
+    }
+
+    @DeleteMapping("/{cpf_cnpj}")
+    public String deleteParceiro(@PathVariable String cpf_cnpj){
+        return parceiroService.deleta(cpf_cnpj);
+    }
+
+    @PutMapping("/{cpf_cnpj}")
+    public String putParceiro(@PathVariable String cpf_cnpj,
+                              @RequestBody ParceiroPutDto dto)
+    {
+        return parceiroService.altera(cpf_cnpj, dto);
+    }
 }
